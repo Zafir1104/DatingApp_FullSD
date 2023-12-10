b45b27c3c2c4ebb20020c81fb3bd32b11401ad74
zafir <zafir@DESKTOP-4UE4H9H>
2023-12-10 23:16:39 +0800

    Add project files.

---
+++ DatingApp_FullSD/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using DatingApp_FullSD.Server.Models;
+using Duende.IdentityServer.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace DatingApp_FullSD.Server.Data
+{
+	public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+	{
+		public ApplicationDbContext(
+			DbContextOptions options,
+			IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+		{
+		}
+	}
+}
---
+++ DatingApp_FullSD/Shared/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace DatingApp_FullSD.Shared
+{
+	public class WeatherForecast
+	{
+		public DateOnly Date { get; set; }
+
+		public int TemperatureC { get; set; }
+
+		public string? Summary { get; set; }
+
+		public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+	}
+}
